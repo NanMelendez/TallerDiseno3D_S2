cae261a1b13a6189071efa8993f1e1e965b79aa3
Luis <x0karimx0@gmail.com>
2026-09-03 18:45:12 -0500

    clase cuadrado hereda de rectangulo

---
+++ Cuadrado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TallerDiseno3D_S2
+{
+    internal class Cuadrado : Rectangulo
+    {
+        public Cuadrado(string nombre, float lado) : base(nombre, lado, lado)
+
+        {
+
+        }
+        
+
+    }
+
+}
--- Rectangulo.cs
+++ Rectangulo.cs
@@ -4,12 +4,13 @@ namespace TallerDiseno3D_S2
     {
         float baseRectangulo;
         float altura;
-        public Rectangulo(float baseRectangulo, float altura)
+        public Rectangulo(string nombre, float baseRectangulo, float altura)
+            : base(nombre)
         {
             this.baseRectangulo = baseRectangulo;
             this.altura = altura;
         }
-        public override float AreaRectangulo()
+        public override float Área()
         {
             return baseRectangulo * altura;
         }
