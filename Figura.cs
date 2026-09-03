14b118698780a2e241a4b2587f4a5e5459e57b8a
Nander Melendez <64971585+NanMelendez@users.noreply.github.com>
2026-09-03 17:49:48 -0500

    Ejercicio 1: Agregar Figura

---
+++ Figura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TallerDiseno3D_S2
+{
+    internal class Figura
+    {
+        public string nombre;
+
+        public Figura(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public virtual float Área()
+        {
+            return 0.0f;
+        }
+    }
+}
