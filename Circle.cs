3597f0506344e07e6f8caa1b7de69627a9e49e92
Alex Zarate <azarate45@gmail.com>
2026-09-03 18:04:48 -0500

    Crear Clase Círculo

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TallerDiseno3D_S2
+{
+    internal class Circle : Figura
+    {
+        float radio;
+        float area;
+        public Circle(string nombre, float radio) : base(nombre)
+        {
+            this.radio = radio;
+        }
+        public override float Área()
+        {
+            area = 3.14f * radio * radio;
+            return area;
+        }
+    }
+}
