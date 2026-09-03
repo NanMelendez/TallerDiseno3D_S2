a284602976d4eec658a18d792204a103382e0655
Nander Melendez <64971585+NanMelendez@users.noreply.github.com>
2026-09-03 17:45:17 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TallerDiseno3D_S2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
